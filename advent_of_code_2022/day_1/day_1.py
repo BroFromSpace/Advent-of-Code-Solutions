23bda3c18d76837a4fd48b4cbe080203b3d331b5
Kyrylo <kyrylo@liveit.io>
2022-12-19 01:19:41 +0000

    Solution for the first day.

---
+++ advent_of_code_2022/day_1/day_1.py
@@ -0,0 +1,45 @@
+def part_1():
+    with open("day_1/input.txt", "r") as f:
+        lines = f.readlines()
+
+    # Go throw each line and find max kkals and current kkals
+    cur_kk = 0
+    max_kk = 0
+    for line in lines:
+        if line != "\n":
+            cur_kk += int(line)
+        else:
+            if cur_kk > max_kk:
+                max_kk = cur_kk
+            cur_kk = 0
+
+    print(max_kk)
+
+
+def part_2():
+    with open("day_1/input.txt", "r") as f:
+        lines = f.readlines()
+
+    # Go throw each line and find max kkals and current kkals
+    arr = []
+    cur_kk = 0
+    max_kk = 0
+    for line in lines:
+        if line != "\n":
+            cur_kk += int(line)
+        else:
+            if cur_kk > max_kk:
+                max_kk = cur_kk
+
+            arr.append(cur_kk)
+            cur_kk = 0
+            
+    # Get sum of 3 biggest kkals items in list
+    total_sum = sum(sorted(arr, reverse=True)[:3])
+
+    print(total_sum)
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
